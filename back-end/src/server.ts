45823cd43bf36d39cff380b4727e0b81d99cede5
thurponto <arthurbrunocosta06@gmail.com>
2024-06-18 23:35:25 -0300

    API REST

---
+++ back-end/src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+import routes from './routes';
+
+const app = express();
+
+
+
+app.use(express.json())
+
+
+app.use(routes)
+
+
+app.listen(3333, () => 'server running on port 3333')
